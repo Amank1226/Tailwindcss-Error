5e0e6977e395dca98a4f5120ba514bf6d582254e
Aman Koushal <77660076+Amank1226@users.noreply.github.com>
2023-05-16 17:49:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
-  content: ["*"],
+  content: ["./src/**/*.{js,html,php}"],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        border: 'border 4s ease infinite',
+      },
+      keyframes: {
+        border: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
